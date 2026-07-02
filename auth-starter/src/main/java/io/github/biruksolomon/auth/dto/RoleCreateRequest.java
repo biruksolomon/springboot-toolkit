2dd5497ee370777851e68603a274100d8885a9a8
Biruk Solomon <biruksolomonmoges@gmail.com>
2026-07-02 15:23:39 +0300

    adding role dtos req and response

---
+++ auth-starter/src/main/java/io/github/biruksolomon/auth/dto/RoleCreateRequest.java
@@ -0,0 +1,18 @@
+package io.github.biruksolomon.auth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class RoleCreateRequest {
+    private String name;
+    private String description;
+    private List<Long> permissionIds;
+}
---
+++ auth-starter/src/main/java/io/github/biruksolomon/auth/dto/RoleResponse.java
@@ -0,0 +1,21 @@
+package io.github.biruksolomon.auth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class RoleResponse {
+    private Long id;
+    private String name;
+    private String description;
+    private List<String> permissions;
+    private LocalDateTime createdAt;
+}
